0a272e8039ec7bb281d3b74d4e65e63c9759439b
Christian Hoffmeister <mail@choffmeister.de>
2015-10-14 15:15:16 +0200

    Add usage example to readme

--- public/data.js
+++ public/data.js
@@ -14,9 +14,7 @@ export default Immutable.fromJS([
       {
         _id: uuid(),
         label: 'Page 1a',
-        children: [
-
-        ]
+        children: []
       },
       {
         _id: uuid(),
