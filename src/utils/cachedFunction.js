eeaaee8fe91658eb2349f33713bd53b74c64c888
Christian Hoffmeister <mail@choffmeister.de>
2015-10-14 13:49:31 +0200

    Add some unit tests

--- src/utils/cachedFunction.js
+++ src/utils/cachedFunction.js
@@ -3,13 +3,15 @@
  * The cached value is return, if all arguments are the _SAME_.
  */
 export default function cachedFunction(fn) {
+  let cached = false;
   let cachedArgs = [];
   let cachedResult = undefined;
 
   return function (...args) {
-    if (args.length === cachedArgs.length && args.every((a, i) => cachedArgs[i] === a)) {
+    if (cached && args.length === cachedArgs.length && args.every((a, i) => cachedArgs[i] === a)) {
       return cachedResult;
     } else {
+      cached = true;
       cachedArgs = args;
       cachedResult = fn.apply(this, args);
       return cachedResult;
--- test/src/NestedList.spec.js
+++ test/src/NestedList.spec.js
@@ -1,5 +1,31 @@
+import Immutable from 'immutable';
+import NestedList from '../../src/NestedList';
+import React from 'react'; // eslint-disable-line no-unused-vars
+import TestUtils from 'react-addons-test-utils';
+
 describe('NestedList', function () {
-  it('TODO', function () {
-    // TODO
+  it('renders children', function () {
+    const onDataChange = chai.spy();
+    const data = Immutable.fromJS([
+      {
+        _id: 1,
+        children: [
+          {_id: 2, children: []},
+          {_id: 3, children: []}
+        ]
+      },
+      {_id: 4, children: []}
+    ]);
+
+    const nestedList = TestUtils.renderIntoDocument(
+      <NestedList className="list" data={data} onDataChange={onDataChange}>
+        {item => <div className="list-item">{item.get('_id')}</div>}
+      </NestedList>
+    );
+
+    const listDiv = TestUtils.findRenderedDOMComponentWithClass(nestedList, 'list');
+    const itemDivs = TestUtils.scryRenderedDOMComponentsWithClass(nestedList, 'list-item');
+    expect(itemDivs).to.have.length(4);
+    expect(itemDivs.every(itemDiv => listDiv.contains(itemDiv))).to.equal(true);
   });
 });
